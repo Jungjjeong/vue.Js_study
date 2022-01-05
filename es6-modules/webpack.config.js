9df10779e147210546b00b50adec747c3ed88c2a
Jungjjeong <sky990106@dgu.ac.kr>
2022-01-05 14:18:49 +0900

    learn Webpack Section 6-7

--- es6-modules/webpack.config.js
+++ es6-modules/webpack.config.js
@@ -1,26 +1,28 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = {
-	mode: 'production',
-	entry: './js/app.js',
-	output: {
-			path: path.resolve(__dirname, 'build'),
-			filename: 'main.bundle.js'
-	},
-	module: {
-		rules: [{
-			test: /\.m?js$/,
-			exclude: /(node_modules|bower_components)/,
-			use: {
-				loader: 'babel-loader',
-				options: {
-					presets: ['@babel/preset-env']
-				}
-			}
-		}]
-	},
-	stats: {
-			colors: true
-	},
-	devtool: 'source-map'
+  mode: "production",
+  entry: "./js/app.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "main.bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  stats: {
+    colors: true,
+  },
+  devtool: "source-map",
 };
